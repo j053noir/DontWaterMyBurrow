f65b3258253cd743451d1d9688b00990e35e289b
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-05 22:08:47 -0500

    feat(data): add structure data scriptable object

---
+++ Assets/Scripts/Data/StructureDataSO.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct StructureCost
+{
+    public ResourceType Type;
+    public int Cost;
+}
+
+[CreateAssetMenu(fileName = "NewStructureData", menuName = "ScriptableObjects/StructureData")]
+public class StructureDataSO : ScriptableObject
+{
+    public GameObject Prefab;
+    public Sprite PreviewSprite;
+    public StructureType Type;
+    public List<StructureCost> Costs;
+    public int MaxHealth;
+}
