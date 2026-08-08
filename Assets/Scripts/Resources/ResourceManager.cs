652e6809a7e51dcb1787aeab6ec28bf77c063aff
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-08 17:44:28 -0500

    feat(resources): add reset resources handling and manager registration

--- Assets/Scripts/Resources/ResourceManager.cs
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -6,6 +6,8 @@ using DontWaterMyBurrow.Data;
 using DontWaterMyBurrow.Building.Events;
 using DontWaterMyBurrow.Structures.Events;
 using DontWaterMyBurrow.Resources.Events;
+using DontWaterMyBurrow.Game.Events;
+using DontWaterMyBurrow.Game;
 
 namespace DontWaterMyBurrow.Resources
 {
@@ -23,6 +25,9 @@ namespace DontWaterMyBurrow.Resources
             EventBus.Subscribe<StructureBuiltEvent>(OnStructureBuilt);
             EventBus.Subscribe<ResourceCollectedEvent>(OnResouceCollected);
             EventBus.Subscribe<RepairStructureRequestEvent>(OnRepairStructureRequested);
+            EventBus.Subscribe<GameStateChangedEvent>(OnGameStateChanged);
+
+            EventBus.Publish(new RegisterManagerEvent(this.GetType()));
         }
 
         private void OnDisable()
@@ -30,79 +35,94 @@ namespace DontWaterMyBurrow.Resources
             EventBus.Unsubscribe<StructureBuiltEvent>(OnStructureBuilt);
             EventBus.Unsubscribe<ResourceCollectedEvent>(OnResouceCollected);
             EventBus.Unsubscribe<RepairStructureRequestEvent>(OnRepairStructureRequested);
+            EventBus.Unsubscribe<GameStateChangedEvent>(OnGameStateChanged);
+
+            EventBus.Publish(new UnregisterManagerEvent(this.GetType()));
         }
 
-    private void OnStructureBuilt(StructureBuiltEvent @event)
-    {
-        ConsumeResources(@event.StructureData);
-    }
-
-    private void OnResouceCollected(ResourceCollectedEvent @event)
-    {
-        AddResource(@event.ResourceType, @event.Quantity);
-    }
-
-    private void OnRepairStructureRequested(RepairStructureRequestEvent @event)
-    {
-        var isValid = HasEnoughResources(@event.StructureSO);
-
-        if (isValid) ConsumeResources(@event.StructureSO);
-
-        @event.Callback?.Invoke(isValid);
-    }
-
-    public void ConsumeResources(StructureDataSO structureSO)
-    {
-        try
+        private void OnStructureBuilt(StructureBuiltEvent @event)
         {
-            if (!HasEnoughResources(structureSO)) return;
+            ConsumeResources(@event.StructureData);
+        }
 
-            foreach (var resource in structureSO.Costs)
+        private void OnResouceCollected(ResourceCollectedEvent @event)
+        {
+            AddResource(@event.ResourceType, @event.Quantity);
+        }
+
+        private void OnRepairStructureRequested(RepairStructureRequestEvent @event)
+        {
+            var isValid = HasEnoughResources(@event.StructureSO);
+
+            if (isValid) ConsumeResources(@event.StructureSO);
+
+            @event.Callback?.Invoke(isValid);
+        }
+
+        private void OnGameStateChanged(GameStateChangedEvent @event)
+        {
+            if (@event.NewState == GameState.Restart) ResetResources();
+        }
+
+        private void ResetResources()
+        {
+            _resources.Clear();
+
+            EventBus.Publish(new ManagerReadyEvent(this.GetType()));
+        }
+
+        public void ConsumeResources(StructureDataSO structureSO)
+        {
+            try
             {
-                if (_resources.ContainsKey(resource.Type))
+                if (!HasEnoughResources(structureSO)) return;
+
+                foreach (var resource in structureSO.Costs)
                 {
-                    _resources[resource.Type] -= resource.Cost;
-                    EventBus.Publish(new ResourceChangedEvent(resource.Type, _resources[resource.Type], -resource.Cost));
+                    if (_resources.ContainsKey(resource.Type))
+                    {
+                        _resources[resource.Type] -= resource.Cost;
+                        EventBus.Publish(new ResourceChangedEvent(resource.Type, _resources[resource.Type], -resource.Cost));
+                    }
                 }
             }
-        }
-        catch (KeyNotFoundException ex)
-        {
-            Debug.LogError($"No resources found for the given resource type: {ex.Message}");
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError($"Error consuming resources: {ex.Message}");
-        }
-    }
-
-    public void AddResource(ResourceType type, int quantity)
-    {
-        // Search for resource in dictionary
-        if (_resources.ContainsKey(type))
-        {
-            _resources[type] += quantity;
-        }
-        // If resource not found, add it to the dictionary
-        else
-        {
-            _resources.Add(type, quantity);
-        }
-
-        EventBus.Publish(new ResourceChangedEvent(type, quantity));
-    }
-
-    public bool HasEnoughResources(StructureDataSO structureSO)
-    {
-        foreach (var resource in structureSO.Costs)
-        {
-            if (!_resources.ContainsKey(resource.Type) || _resources[resource.Type] < resource.Cost)
+            catch (KeyNotFoundException ex)
             {
-                Debug.LogWarning("Insufficient resources to build the structure, missing " + resource.Type + " with cost " + resource.Cost);
-                return false;
+                Debug.LogError($"No resources found for the given resource type: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Error consuming resources: {ex.Message}");
             }
         }
-        return true;
+
+        public void AddResource(ResourceType type, int quantity)
+        {
+            // Search for resource in dictionary
+            if (_resources.ContainsKey(type))
+            {
+                _resources[type] += quantity;
+            }
+            // If resource not found, add it to the dictionary
+            else
+            {
+                _resources.Add(type, quantity);
+            }
+
+            EventBus.Publish(new ResourceChangedEvent(type, _resources[type], quantity));
+        }
+
+        public bool HasEnoughResources(StructureDataSO structureSO)
+        {
+            foreach (var resource in structureSO.Costs)
+            {
+                if (!_resources.ContainsKey(resource.Type) || _resources[resource.Type] < resource.Cost)
+                {
+                    Debug.LogWarning("Insufficient resources to build the structure, missing " + resource.Type + " with cost " + resource.Cost);
+                    return false;
+                }
+            }
+            return true;
+        }
     }
-}
 }
