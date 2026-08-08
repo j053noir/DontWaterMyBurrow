8c1bdab052c6e8d7b0aa4c92a8a76f3ff3d6ea5a
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-08 17:41:36 -0500

    feat(wave): add restart reset and registration to wave manager

--- Assets/Scripts/Wave/Events/WaveTimerChangedEvent.cs
+++ Assets/Scripts/Wave/Events/WaveTimerChangedEvent.cs
@@ -4,11 +4,13 @@ namespace DontWaterMyBurrow.Wave.Events
 {
     public readonly struct WaveTimerChangedEvent
     {
+        public readonly int WaveNumber;
         public readonly float TimeUntilNextWave;
         public readonly float WaveTimer;
 
-        public WaveTimerChangedEvent(float timeUntilNextWave, float waveTimer)
+        public WaveTimerChangedEvent(int waveNumber, float timeUntilNextWave, float waveTimer)
         {
+            WaveNumber = waveNumber;
             TimeUntilNextWave = timeUntilNextWave;
             WaveTimer = waveTimer;
         }
--- Assets/Scripts/Wave/WaveManager.cs
+++ Assets/Scripts/Wave/WaveManager.cs
@@ -4,7 +4,6 @@ using DontWaterMyBurrow.Data;
 using DontWaterMyBurrow.Game;
 using DontWaterMyBurrow.Game.Events;
 using DontWaterMyBurrow.Wave.Events;
-using DontWaterMyBurrow.Hazards;
 using UnityEngine;
 using System.Collections.Generic;
 
@@ -12,86 +11,114 @@ namespace DontWaterMyBurrow.Wave
 {
     public class WaveManager : MonoBehaviour
     {
-    [Header("Wave")]
-    [SerializeField] private WaveDataSO _currentWaveData; // Use scriptable object
-    [SerializeField] private float _timeUntilNextWave = 10f;
-    [SerializeField] private bool _isWaveActive = false;
+        [Header("Wave")]
+        [SerializeField] private WaveDataSO _firstWaveData;
+        [SerializeField] private WaveDataSO _currentWaveData;
+        [SerializeField] private float _timeUntilNextWave = 10f;
+        [SerializeField] private bool _isWaveActive = false;
 
-    [Header("References")]
-    [SerializeField] private GridManager _gridManager;
+        [Header("References")]
+        [SerializeField] private GridManager _gridManager;
 
-    private float _waveTimer = 0f;
-    private Dictionary<HazardsSpawnData, float> _hazardTimers = new();
+        private float _waveTimer = 0f;
+        private Dictionary<HazardsSpawnData, float> _hazardTimers;
 
-    private void OnEnable()
-    {
-        EventBus.Subscribe<GameStateChangedEvent>(OnGameStateChanged);
-    }
-
-    private void OnDisable()
-    {
-        EventBus.Unsubscribe<GameStateChangedEvent>(OnGameStateChanged);
-    }
-
-    private void OnGameStateChanged(GameStateChangedEvent @event)
-    {
-        if (@event.NewState == GameState.WavePreparation)
+        private void Awake()
         {
-            StartWave();
-        }
-    }
-
-    private void Update()
-    {
-        if (!_isWaveActive) return;
-
-        _waveTimer -= Time.deltaTime;
-        EventBus.Publish(new WaveTimerChangedEvent(_timeUntilNextWave, _waveTimer));
-
-        if (_waveTimer <= 0)
-        {
-            EndWave();
-            return;
+            _hazardTimers = new();
         }
 
-        // Spawn hazards based on their intervals
-        foreach (var hazard in _currentWaveData.HazardsToSpawn)
+        private void OnEnable()
         {
-            _hazardTimers[hazard] += Time.deltaTime;
-            if (_hazardTimers[hazard] >= hazard.SpawnInterval)
+            EventBus.Subscribe<GameStateChangedEvent>(OnGameStateChanged);
+            EventBus.Publish(new RegisterManagerEvent(this.GetType()));
+        }
+
+        private void OnDisable()
+        {
+            EventBus.Unsubscribe<GameStateChangedEvent>(OnGameStateChanged);
+            EventBus.Publish(new UnregisterManagerEvent(this.GetType()));
+        }
+
+        private void OnGameStateChanged(GameStateChangedEvent @event)
+        {
+            if (@event.NewState == GameState.WavePreparation)
             {
-                SpawnThreat(hazard);
-                _hazardTimers[hazard] = 0f;
+                StartWave();
+            }
+            else if (@event.NewState == GameState.Restart)
+            {
+                ResetWave();
             }
         }
-    }
 
-    public void StartWave()
-    {
-        _isWaveActive = true;
-        _waveTimer = _currentWaveData.WaveDuration;
-        _hazardTimers.Clear();
-
-        foreach (var hazard in _currentWaveData.HazardsToSpawn)
+        private void Update()
         {
-            _hazardTimers[hazard] = 0f;
+            if (!_isWaveActive) return;
+
+            _waveTimer -= Time.deltaTime;
+            EventBus.Publish(new WaveTimerChangedEvent(_currentWaveData.WaveNumber, _timeUntilNextWave, _waveTimer));
+
+            if (_waveTimer <= 0)
+            {
+                EndWave();
+                return;
+            }
+
+            SpawnHazards();
         }
 
-        EventBus.Publish(new WaveStartedEvent(_currentWaveData.WaveNumber));
-    }
+        private void ResetWave()
+        {
+            _currentWaveData = _firstWaveData;
+            _timeUntilNextWave = 10f;
+            _isWaveActive = false;
+            _waveTimer = 0f;
+            _hazardTimers.Clear();
+            EventBus.Publish(new ManagerReadyEvent(this.GetType()));
+        }
 
-    public void EndWave()
-    {
-        _isWaveActive = false;
-        EventBus.Publish(new WaveCompletedEvent());
-    }
+        private void SpawnHazards()
+        {
+            // Spawn hazards based on their intervals
+            foreach (var hazard in _currentWaveData.HazardsToSpawn)
+            {
+                _hazardTimers[hazard] += Time.deltaTime;
+                if (_hazardTimers[hazard] >= hazard.SpawnInterval)
+                {
+                    SpawnThreat(hazard);
+                    _hazardTimers[hazard] = 0f;
+                }
+            }
+        }
 
-    public void SpawnThreat(HazardsSpawnData hazard)
-    {
-        var spawnPosition = new Vector3(Random.Range(_gridManager.MinXBoundary, _gridManager.MaxXBoundary), _gridManager.YBottomBoundary, 0);
-        Instantiate(hazard.Prefab, spawnPosition, Quaternion.identity);
-        var cellPosition = new Vector2Int(Mathf.RoundToInt(spawnPosition.x), Mathf.RoundToInt(spawnPosition.y));
-        EventBus.Publish(new HazardSpawnedEvent(hazard.Type, cellPosition));
+        public void StartWave()
+        {
+            _isWaveActive = true;
+            _timeUntilNextWave = 10f;
+            _waveTimer = _currentWaveData.WaveDuration;
+            _hazardTimers.Clear();
+
+            foreach (var hazard in _currentWaveData.HazardsToSpawn)
+            {
+                _hazardTimers[hazard] = 0f;
+            }
+
+            EventBus.Publish(new WaveStartedEvent(_currentWaveData.WaveNumber));
+        }
+
+        public void EndWave()
+        {
+            _isWaveActive = false;
+            EventBus.Publish(new WaveCompletedEvent());
+        }
+
+        public void SpawnThreat(HazardsSpawnData hazard)
+        {
+            var spawnPosition = new Vector3(Random.Range(_gridManager.MinXBoundary, _gridManager.MaxXBoundary), _gridManager.YBottomBoundary, 0);
+            Instantiate(hazard.Prefab, spawnPosition, Quaternion.identity);
+            var cellPosition = new Vector2Int(Mathf.RoundToInt(spawnPosition.x), Mathf.RoundToInt(spawnPosition.y));
+            EventBus.Publish(new HazardSpawnedEvent(hazard.Type, cellPosition));
+        }
     }
-}
 }
