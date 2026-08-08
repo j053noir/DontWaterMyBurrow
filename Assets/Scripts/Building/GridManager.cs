c242474edfde95d5c8a1251dd1eb34decea4f3f3
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-08 17:44:45 -0500

    feat(building): add grid clear on restart and grid manager registration

--- Assets/Scripts/Building/GridManager.cs
+++ Assets/Scripts/Building/GridManager.cs
@@ -5,6 +5,8 @@ using DontWaterMyBurrow.Core;
 using DontWaterMyBurrow.Building.Events;
 using DontWaterMyBurrow.Structures.Events;
 using DontWaterMyBurrow.Resources.Events;
+using DontWaterMyBurrow.Game.Events;
+using DontWaterMyBurrow.Game;
 
 namespace DontWaterMyBurrow.Building
 {
@@ -29,94 +31,121 @@ namespace DontWaterMyBurrow.Building
 
     public class GridManager : MonoBehaviour
     {
-    [SerializeField] private Vector2Int _burrowPosition;
+        [SerializeField] private Vector2Int _burrowPosition;
 
-    private Dictionary<Vector2Int, GridObject> _occupiedCells;
+        private Dictionary<Vector2Int, GridObject> _occupiedCells;
 
-    [Header("Boundaries")]
-    [SerializeField] private int _minXBoundary;
-    [SerializeField] private int _maxXBoundary;
-    [SerializeField] private int _yBottomBoundary;
-    [SerializeField] private int _yTopBoundary;
+        [Header("Boundaries")]
+        [SerializeField] private int _minXBoundary;
+        [SerializeField] private int _maxXBoundary;
+        [SerializeField] private int _yBottomBoundary;
+        [SerializeField] private int _yTopBoundary;
 
-    public Vector2Int BurrowPosition => _burrowPosition;
+        public Vector2Int BurrowPosition => _burrowPosition;
 
-    public int MinXBoundary => _minXBoundary;
-    public int MaxXBoundary => _maxXBoundary;
-    public int YBottomBoundary => _yBottomBoundary;
-    public int YTopBoundary => _yTopBoundary;
+        public int MinXBoundary => _minXBoundary;
+        public int MaxXBoundary => _maxXBoundary;
+        public int YBottomBoundary => _yBottomBoundary;
+        public int YTopBoundary => _yTopBoundary;
 
-    private void Awake()
-    {
-        _occupiedCells = new();
+        private void Awake()
+        {
+            _occupiedCells = new();
+        }
+
+        private void OnEnable()
+        {
+            EventBus.Subscribe<StructureBuiltEvent>(OnStructureBuilt);
+            EventBus.Subscribe<StructureDestroyedEvent>(OnStructureDestroyed);
+            EventBus.Subscribe<DamCreatedEvent>(OnDamCreated);
+            EventBus.Subscribe<DamDestroyedEvent>(OnDamDestroyed);
+            EventBus.Subscribe<ResourceSpawnedEvent>(OnResourceSpawned);
+            EventBus.Subscribe<ResourceCollectedEvent>(OnResourceCollected);
+            EventBus.Subscribe<GameStateChangedEvent>(OnGameStateChanged);
+
+            EventBus.Publish(new RegisterManagerEvent(this.GetType()));
+        }
+
+        private void OnDisable()
+        {
+            EventBus.Unsubscribe<StructureBuiltEvent>(OnStructureBuilt);
+            EventBus.Unsubscribe<StructureDestroyedEvent>(OnStructureDestroyed);
+            EventBus.Unsubscribe<DamCreatedEvent>(OnDamCreated);
+            EventBus.Unsubscribe<DamDestroyedEvent>(OnDamDestroyed);
+            EventBus.Unsubscribe<ResourceSpawnedEvent>(OnResourceSpawned);
+            EventBus.Unsubscribe<ResourceCollectedEvent>(OnResourceCollected);
+            EventBus.Unsubscribe<GameStateChangedEvent>(OnGameStateChanged);
+
+            EventBus.Publish(new UnregisterManagerEvent(this.GetType()));
+        }
+
+        private void OnStructureBuilt(StructureBuiltEvent @event)
+        {
+            var gridObject = new GridObject(@event.StructureInstance, GridCellType.Structure);
+            _occupiedCells.Add(@event.Position, gridObject);
+        }
+
+        private void OnStructureDestroyed(StructureDestroyedEvent @event)
+        {
+            _occupiedCells.Remove(@event.Position);
+        }
+
+        private void OnDamCreated(DamCreatedEvent @event)
+        {
+            var gridObject = new GridObject(@event.Instance, GridCellType.Dam);
+            _occupiedCells.Add(@event.Position, gridObject);
+        }
+
+        private void OnDamDestroyed(DamDestroyedEvent @event)
+        {
+            _occupiedCells.Remove(@event.Position);
+        }
+
+        private void OnResourceSpawned(ResourceSpawnedEvent @event)
+        {
+            var gridObject = new GridObject(@event.Instance, GridCellType.Resource);
+            _occupiedCells.Add(@event.Position, gridObject);
+        }
+
+        private void OnResourceCollected(ResourceCollectedEvent @event)
+        {
+            _occupiedCells.Remove(@event.Position);
+        }
+
+        private void OnGameStateChanged(GameStateChangedEvent @event)
+        {
+            if (@event.NewState == GameState.Restart)
+            {
+                ClearGrid();
+            }
+        }
+
+        private void ClearGrid()
+        {
+            foreach (var item in _occupiedCells)
+            {
+                // TODO: Return instance to pool
+                Destroy(item.Value.Instance);
+            }
+
+            _occupiedCells.Clear();
+
+            EventBus.Publish(new ManagerReadyEvent(this.GetType()));
+        }
+
+        public bool IsCellOccupied(Vector2Int position)
+        {
+            return _occupiedCells.ContainsKey(position);
+        }
+
+        public bool IsCellDam(Vector2Int position)
+        {
+            return _occupiedCells.ContainsKey(position) && _occupiedCells[position].Type == GridCellType.Dam;
+        }
+
+        public bool IsCellOccupiedByStructure(Vector2Int position)
+        {
+            return _occupiedCells.ContainsKey(position) && _occupiedCells[position].Type == GridCellType.Structure;
+        }
     }
-
-    private void OnEnable()
-    {
-        EventBus.Subscribe<StructureBuiltEvent>(OnStructureBuilt);
-        EventBus.Subscribe<StructureDestroyedEvent>(OnStructureDestroyed);
-        EventBus.Subscribe<DamCreatedEvent>(OnDamCreated);
-        EventBus.Subscribe<DamDestroyedEvent>(OnDamDestroyed);
-        EventBus.Subscribe<ResourceSpawnedEvent>(OnResourceSpawned);
-        EventBus.Subscribe<ResourceCollectedEvent>(OnResourceCollected);
-    }
-
-    private void OnDisable()
-    {
-        EventBus.Unsubscribe<StructureBuiltEvent>(OnStructureBuilt);
-        EventBus.Unsubscribe<StructureDestroyedEvent>(OnStructureDestroyed);
-        EventBus.Unsubscribe<DamCreatedEvent>(OnDamCreated);
-        EventBus.Unsubscribe<DamDestroyedEvent>(OnDamDestroyed);
-        EventBus.Unsubscribe<ResourceSpawnedEvent>(OnResourceSpawned);
-        EventBus.Unsubscribe<ResourceCollectedEvent>(OnResourceCollected);
-    }
-
-    private void OnStructureBuilt(StructureBuiltEvent @event)
-    {
-        var gridObject = new GridObject(@event.StructureInstance, GridCellType.Structure);
-        _occupiedCells.Add(@event.Position, gridObject);
-    }
-
-    private void OnStructureDestroyed(StructureDestroyedEvent @event)
-    {
-        _occupiedCells.Remove(@event.Position);
-    }
-
-    private void OnDamCreated(DamCreatedEvent @event)
-    {
-        var gridObject = new GridObject(@event.Instance, GridCellType.Dam);
-        _occupiedCells.Add(@event.Position, gridObject);
-    }
-
-    private void OnDamDestroyed(DamDestroyedEvent @event)
-    {
-        _occupiedCells.Remove(@event.Position);
-    }
-
-    private void OnResourceSpawned(ResourceSpawnedEvent @event)
-    {
-        var gridObject = new GridObject(@event.Instance, GridCellType.Resource);
-        _occupiedCells.Add(@event.Position, gridObject);
-    }
-
-    private void OnResourceCollected(ResourceCollectedEvent @event)
-    {
-        _occupiedCells.Remove(@event.Position);
-    }
-
-    public bool IsCellOccupied(Vector2Int position)
-    {
-        return _occupiedCells.ContainsKey(position);
-    }
-
-    public bool IsCellDam(Vector2Int position)
-    {
-        return _occupiedCells.ContainsKey(position) && _occupiedCells[position].Type == GridCellType.Dam;
-    }
-
-    public bool IsCellOccupiedByStructure(Vector2Int position)
-    {
-        return _occupiedCells.ContainsKey(position) && _occupiedCells[position].Type == GridCellType.Structure;
-    }
-}
 }
