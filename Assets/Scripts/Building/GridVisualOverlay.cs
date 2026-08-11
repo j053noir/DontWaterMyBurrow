bccda51173ef565cb2e740834442ed96323835fc
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-11 17:24:45 -0500

    feat(building): add GridVisualOverlay component for rendering 2D ground grid

---
+++ Assets/Scripts/Building/GridVisualOverlay.cs
@@ -0,0 +1,71 @@
+using DontWaterMyBurrow.Core;
+using DontWaterMyBurrow.Data;
+using DontWaterMyBurrow.Game;
+using DontWaterMyBurrow.Game.Events;
+using UnityEngine;
+
+namespace DontWaterMyBurrow.Building
+{
+    public class GridVisualOverlay : MonoBehaviour
+    {
+        [Header("Config")]
+        [SerializeField] private MapGridConfigSO _mapGridConfig;
+
+        [Header("Visuals")]
+        [SerializeField] private Sprite _tileSprite;
+        [SerializeField] private Color _gridColor = new Color(1f, 1f, 1f, 0.15f);
+        [SerializeField] private string _sortingLayerName = "Default";
+        [SerializeField] private int _sortingOrder = -1;
+
+        private Transform _gridParent;
+
+        private void Awake()
+        {
+            _gridParent = new GameObject("VisualGridContainer").transform;
+            _gridParent.SetParent(transform);
+            _gridParent.position = Vector3.zero;
+            GenerateGrid();
+        }
+
+        private void OnEnable()
+        {
+            EventBus.Subscribe<GameStateChangedEvent>(OnGameStateChanged);
+        }
+
+        private void OnDisable()
+        {
+            EventBus.Unsubscribe<GameStateChangedEvent>(OnGameStateChanged);
+        }
+
+        private void OnGameStateChanged(GameStateChangedEvent @event)
+        {
+            bool isVisible = @event.NewState == GameState.WavePreparation || @event.NewState == GameState.WaveActive;
+            _gridParent.gameObject.SetActive(isVisible);
+        }
+
+        private void GenerateGrid()
+        {
+            for (int x = _mapGridConfig.MinXBoundary; x <= _mapGridConfig.MaxXBoundary; x++)
+            {
+                for (int y = _mapGridConfig.YBottomBoundary; y <= _mapGridConfig.YTopBoundary; y++)
+                {
+                    var tile = new GameObject($"Tile_{x}_{y}");
+                    tile.transform.SetParent(_gridParent);
+                    tile.transform.position = new Vector3(x * _mapGridConfig.TileSize, y * _mapGridConfig.TileSize, 0);
+
+                    if (_tileSprite != null)
+                    {
+                        float sprideWidth = _tileSprite.bounds.size.x;
+                        float sprideHeight = _tileSprite.bounds.size.y;
+                        //tile.transform.localScale = new Vector3(sprideWidth / _mapGridConfig.TileSize, sprideHeight / _mapGridConfig.TileSize, 1);
+                        var spriteRenderer = tile.AddComponent<SpriteRenderer>();
+                        spriteRenderer.sprite = _tileSprite;
+                        spriteRenderer.color = _gridColor;
+                        spriteRenderer.sortingOrder = _sortingOrder;
+                        spriteRenderer.sortingLayerName = _sortingLayerName;
+                    }
+                }
+            }
+        }
+    }
+}
