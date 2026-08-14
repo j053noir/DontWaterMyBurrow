b77cf1284bea0ebed1b3a8d9d32bc6806a0b0edd
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-13 19:02:04 -0500

    feat(structures): add random grid positioning for drain structure

--- Assets/Scripts/Structures/DrainController.cs
+++ Assets/Scripts/Structures/DrainController.cs
@@ -5,12 +5,16 @@ using DontWaterMyBurrow.Hazards;
 using DontWaterMyBurrow.Wave.Events;
 using DontWaterMyBurrow.Water.Events;
 using DontWaterMyBurrow.Structures.State;
+using DontWaterMyBurrow.Data;
 
 namespace DontWaterMyBurrow.Structures
 {
     [RequireComponent(typeof(Collider2D))]
     public class DrainController : MonoBehaviour
     {
+        [Header("Config")]
+        [SerializeField] private MapGridConfigSO _mapGridConfig;
+
         [Header("Setup")]
         [SerializeField] private Vector2Int _position;
         [SerializeField] private int _drainAmount;
@@ -37,6 +41,19 @@ namespace DontWaterMyBurrow.Structures
             UncloggedState = new DrainUncloggedState(this);
 
             _stateMachine.ChangeState(UncloggedState);
+        }
+
+        private void Start()
+        {
+            if (_mapGridConfig == null) Debug.LogError("MapGridConfig is not assigned to DrainController");
+
+            _position = SetDrainPosition();
+
+            transform.position = new Vector3(
+                _position.x * _mapGridConfig.TileSize,
+                _position.y * _mapGridConfig.TileSize,
+                0
+            );
         }
 
         private void OnEnable()
@@ -80,6 +97,31 @@ namespace DontWaterMyBurrow.Structures
         public void CleanDrain()
         {
             _stateMachine.ChangeState(UncloggedState);
+        }
+
+        private Vector2Int SetDrainPosition()
+        {
+            var x = GenerateRandomX();
+            var y = GenerateRandomY();
+
+            // Avoid placing drain at the same position as the burrow
+            while (x == _mapGridConfig.BurrowPosition.x && y == _mapGridConfig.BurrowPosition.y)
+            {
+                x = GenerateRandomX();
+                y = GenerateRandomY();
+            }
+
+            return new Vector2Int(x, y);
+        }
+
+        private int GenerateRandomX()
+        {
+            return UnityEngine.Random.Range(_mapGridConfig.MinXBoundary + 1, _mapGridConfig.MaxXBoundary - 1);
+        }
+
+        private int GenerateRandomY()
+        {
+            return UnityEngine.Random.Range(_mapGridConfig.MinYBoundary + _mapGridConfig.MaxYBoundary / 2, _mapGridConfig.MaxYBoundary - 1);
         }
     }
 }
