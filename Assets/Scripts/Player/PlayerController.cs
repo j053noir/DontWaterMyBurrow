d3e7c5dd22310145b05ee16ece1c087f466ff631
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-18 22:37:14 -0500

    feat(player): implement grid-based tile movement using WorldGridDataSO

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,18 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-using DontWaterMyBurrow.Core;
-using DontWaterMyBurrow.Player.Events;
+using System.Collections.Generic;
 using DontWaterMyBurrow.Building.Events;
-using DontWaterMyBurrow.Structures;
-using DontWaterMyBurrow.Structures.Events;
+using DontWaterMyBurrow.Core;
+using DontWaterMyBurrow.Data;
+using DontWaterMyBurrow.Game;
+using DontWaterMyBurrow.Game.Events;
+using DontWaterMyBurrow.Player.Events;
+using DontWaterMyBurrow.Player.States;
 using DontWaterMyBurrow.Resources;
 using DontWaterMyBurrow.Resources.Events;
-using DontWaterMyBurrow.Player.States;
-using DontWaterMyBurrow.Game.Events;
-using DontWaterMyBurrow.Game;
-using DontWaterMyBurrow.Data;
+using DontWaterMyBurrow.Structures;
+using DontWaterMyBurrow.Structures.Events;
 using DontWaterMyBurrow.UI.Events;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace DontWaterMyBurrow.Player
 {
@@ -22,23 +23,32 @@ namespace DontWaterMyBurrow.Player
     {
         [Header("Config")]
         [SerializeField] private MapGridConfigSO _mapGridConfig;
+        [SerializeField] private WorldGridDataSO _worldGridData;
         [SerializeField] private TransformAnchorSO _playerTransformAnchor;
 
         [Header("Movement")]
+        [SerializeField] private Vector2Int _currentCell;
+        [SerializeField] private Vector2Int _targetCell;
+        [SerializeField] private Vector2Int _facingDirection = Vector2Int.down;
+
+        [Header("Movement Speed")]
         [SerializeField] private float _baseMoveSpeed = 5f;
         [SerializeField] private float _currentMoveSpeed = 5f;
-        [SerializeField] private Vector2 _moveInput;
-        [SerializeField] private Vector2 _facingDirection = Vector2.down;
+
+        [Header("Actions")]
         [SerializeField] private float _interactionDistance = 1f;
-        [SerializeField] private Vector2Int _targetCell;
         [SerializeField] private int _repairAmount = 10;
 
         [Header("Debug")]
         [SerializeField] private bool _debugMode = false;
         public bool DebugMode => _debugMode;
 
+        private Vector2Int _moveDirection;
+        private bool _isMoving;
+
         private Rigidbody2D _rigidBody2d;
 
+        #region States
         public StateMachine StateMachine;
         public PlayerDisabledState DisabledState { get; private set; }
         public PlayerNormalState NormalState { get; private set; }
@@ -46,10 +56,12 @@ namespace DontWaterMyBurrow.Player
         public PlayerUncloggingState UncloggingState { get; private set; }
         public PlayerBuildMenuState BuildMenuState { get; private set; }
         public PlayerPlacementState PlacementState { get; private set; }
+        #endregion
 
         private void Awake()
         {
             StateMachine = new();
+
             DisabledState = new PlayerDisabledState(this);
             NormalState = new PlayerNormalState(this, _baseMoveSpeed);
             MudState = new PlayerMudState(this, _baseMoveSpeed * 0.5f);
@@ -57,36 +69,38 @@ namespace DontWaterMyBurrow.Player
             _rigidBody2d = GetComponent<Rigidbody2D>();
 
             if (_mapGridConfig == null) Debug.LogError("[PlayerController] MapGridConfigSO is null");
+            if (_worldGridData == null) Debug.LogError("[PlayerController] WorldGridDataSO is null");
             if (_playerTransformAnchor == null) Debug.LogError("[PlayerController] PlayerTransformAnchorSO is null");
         }
 
         private void Start()
         {
+            _currentCell = _mapGridConfig.WorldToGrid(transform.position);
             StateMachine.ChangeState(DisabledState);
         }
 
         private void OnEnable()
         {
-            EventBus.Subscribe<GameStateChangedEvent>(OnGameStateChanged);
-            EventBus.Subscribe<PlayerOnMudEvent>(OnPlayerOnMudEvent);
-            EventBus.Subscribe<StructureBuiltEvent>(OnStructureBuilt);
             EventBus.Subscribe<BuildFailedEvent>(OnBuildFailed);
-            EventBus.Subscribe<SelectStructureToBuildEvent>(OnSelectStructureToBuildEvent);
             EventBus.Subscribe<ClosedBuildMenuEvent>(OnClosedBuildMenuEvent);
+            EventBus.Subscribe<GameStateChangedEvent>(OnGameStateChanged);
             EventBus.Subscribe<OutOfResourcesEvent>(OnOutOfResources);
+            EventBus.Subscribe<PlayerOnMudEvent>(OnPlayerOnMudEvent);
+            EventBus.Subscribe<SelectStructureToBuildEvent>(OnSelectStructureToBuildEvent);
+            EventBus.Subscribe<StructureBuiltEvent>(OnStructureBuilt);
 
             _playerTransformAnchor.Transform = transform;
         }
 
         private void OnDisable()
         {
-            EventBus.Unsubscribe<GameStateChangedEvent>(OnGameStateChanged);
-            EventBus.Unsubscribe<PlayerOnMudEvent>(OnPlayerOnMudEvent);
-            EventBus.Unsubscribe<StructureBuiltEvent>(OnStructureBuilt);
             EventBus.Unsubscribe<BuildFailedEvent>(OnBuildFailed);
-            EventBus.Unsubscribe<SelectStructureToBuildEvent>(OnSelectStructureToBuildEvent);
             EventBus.Unsubscribe<ClosedBuildMenuEvent>(OnClosedBuildMenuEvent);
+            EventBus.Unsubscribe<GameStateChangedEvent>(OnGameStateChanged);
             EventBus.Unsubscribe<OutOfResourcesEvent>(OnOutOfResources);
+            EventBus.Unsubscribe<PlayerOnMudEvent>(OnPlayerOnMudEvent);
+            EventBus.Unsubscribe<SelectStructureToBuildEvent>(OnSelectStructureToBuildEvent);
+            EventBus.Unsubscribe<StructureBuiltEvent>(OnStructureBuilt);
 
             _playerTransformAnchor.Transform = null;
         }
@@ -100,94 +114,6 @@ namespace DontWaterMyBurrow.Player
         {
             PlayerMove();
             StateMachine.FixedUpdate();
-        }
-
-        private void PlayerMove()
-        {
-            Vector2 nextPosition = _rigidBody2d.position + _moveInput * (_currentMoveSpeed * Time.fixedDeltaTime);
-
-            if (_mapGridConfig != null)
-            {
-                nextPosition.x = Mathf.Clamp(nextPosition.x, _mapGridConfig.MinXBoundary, _mapGridConfig.MaxXBoundary);
-                nextPosition.y = Mathf.Clamp(nextPosition.y, _mapGridConfig.MinYBoundary, _mapGridConfig.MaxYBoundary);
-            }
-
-            _rigidBody2d.MovePosition(nextPosition);
-        }
-
-        private void OnGameStateChanged(GameStateChangedEvent @event)
-        {
-            if (@event.NewState == GameState.MainMenu ||
-                @event.NewState == GameState.GameOver ||
-                @event.NewState == GameState.Victory ||
-                @event.NewState == GameState.Pause)
-            {
-                StateMachine.ChangeState(DisabledState);
-            }
-            else if (@event.NewState == GameState.WavePreparation || @event.NewState == GameState.WaveActive)
-            {
-                if (_debugMode) Debug.Log($"[PlayerController] Game state changed to {@event.NewState}, Time Scale: {Time.timeScale}");
-                StateMachine.ChangeState(NormalState);
-            }
-        }
-
-        private void OnPlayerOnMudEvent(PlayerOnMudEvent @event)
-        {
-            StateMachine.ChangeState(@event.OnMud ? MudState : NormalState);
-        }
-
-        private void OnBuildFailed(BuildFailedEvent @event)
-        {
-            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
-            {
-                StateMachine.ChangeState(buildState.PreviousState);
-            }
-        }
-
-        private void OnStructureBuilt(StructureBuiltEvent @event)
-        {
-            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
-            {
-                StateMachine.ChangeState(buildState.PreviousState);
-            }
-        }
-
-        private void OnSelectStructureToBuildEvent(SelectStructureToBuildEvent @event)
-        {
-            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
-            {
-                PlacementState = new PlayerPlacementState(this, @event.StructureData, buildState.PreviousState);
-                StateMachine.ChangeState(PlacementState);
-            }
-        }
-
-        private void OnClosedBuildMenuEvent(ClosedBuildMenuEvent @event)
-        {
-            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
-            {
-                StateMachine.ChangeState(buildState.PreviousState);
-            }
-        }
-
-        private void OnOutOfResources(OutOfResourcesEvent @event)
-        {
-            if (StateMachine.CurrentState is PlayerPlacementState placementState && placementState.StructureData == @event.StructureData)
-            {
-                StateMachine.ChangeState(placementState.PreviousState);
-            }
-        }
-
-        private Vector2Int TargetCell()
-        {
-            var currentCell = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
-            var facingCell = new Vector2Int(Mathf.RoundToInt(_facingDirection.x), Mathf.RoundToInt(_facingDirection.y));
-
-            return currentCell + facingCell;
-        }
-
-        public void SetMoveSpeed(float moveSpeed)
-        {
-            _currentMoveSpeed = moveSpeed;
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -206,26 +132,131 @@ namespace DontWaterMyBurrow.Player
             }
         }
 
+        #region Event Handlers
+
+        private void OnBuildFailed(BuildFailedEvent @event)
+        {
+            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
+            {
+                StateMachine.ChangeState(buildState.PreviousState);
+            }
+        }
+
+        private void OnClosedBuildMenuEvent(ClosedBuildMenuEvent @event)
+        {
+            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
+            {
+                StateMachine.ChangeState(buildState.PreviousState);
+            }
+        }
+
+        private void OnGameStateChanged(GameStateChangedEvent @event)
+        {
+            if (@event.NewState == GameState.MainMenu ||
+                @event.NewState == GameState.GameOver ||
+                @event.NewState == GameState.Victory ||
+                @event.NewState == GameState.Pause)
+            {
+                StateMachine.ChangeState(DisabledState);
+            }
+            else if (@event.NewState == GameState.WavePreparation || @event.NewState == GameState.WaveActive)
+            {
+                if (_debugMode) Debug.Log($"[PlayerController] Game state changed to {@event.NewState}, Time Scale: {Time.timeScale}");
+                StateMachine.ChangeState(NormalState);
+            }
+        }
+
+        private void OnOutOfResources(OutOfResourcesEvent @event)
+        {
+            if (StateMachine.CurrentState is PlayerPlacementState placementState && placementState.StructureData == @event.StructureData)
+            {
+                StateMachine.ChangeState(placementState.PreviousState);
+            }
+        }
+
+        private void OnPlayerOnMudEvent(PlayerOnMudEvent @event)
+        {
+            StateMachine.ChangeState(@event.OnMud ? MudState : NormalState);
+        }
+
+        private void OnSelectStructureToBuildEvent(SelectStructureToBuildEvent @event)
+        {
+            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
+            {
+                PlacementState = new PlayerPlacementState(this, @event.StructureData, buildState.PreviousState);
+                StateMachine.ChangeState(PlacementState);
+            }
+        }
+
+        private void OnStructureBuilt(StructureBuiltEvent @event)
+        {
+            if (StateMachine.CurrentState is PlayerBuildMenuState buildState)
+            {
+                StateMachine.ChangeState(buildState.PreviousState);
+            }
+        }
+        #endregion
+
+        private void PlayerMove()
+        {
+            if (_isMoving)
+            {
+                var targetWorldPosition = _mapGridConfig.GridToWorld(_targetCell);
+
+                var nextPos = Vector2.MoveTowards(_rigidBody2d.position, targetWorldPosition, _currentMoveSpeed * Time.fixedDeltaTime);
+                _rigidBody2d.MovePosition(nextPos);
+
+                if (Vector2.Distance(targetWorldPosition, _rigidBody2d.position) < 0.001f)
+                {
+                    _rigidBody2d.position = targetWorldPosition;
+                    _currentCell = _targetCell;
+                    _isMoving = false;
+                    _targetCell = _currentCell + _facingDirection;
+                }
+            }
+            else if (_moveDirection != Vector2Int.zero)
+            {
+                var desiredCell = _currentCell + _moveDirection;
+                if (_mapGridConfig.IsWithinBounds(desiredCell) && _worldGridData.IsWalkable(desiredCell))
+                {
+                    _targetCell = desiredCell;
+                    _isMoving = true;
+                }
+                else
+                {
+                    _isMoving = false;
+                }
+            }
+        }
+
+        private Vector2Int TargetCell()
+        {
+            var currentCell = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+
+            return currentCell + _moveDirection;
+        }
+
+        # region Input Actions
         public void OnMove(InputValue value)
         {
             var rawInput = value.Get<Vector2>();
 
             if (Mathf.Abs(rawInput.y) > 0.01f && Mathf.Abs(rawInput.y) >= Mathf.Abs(rawInput.x))
             {
-                _moveInput = new Vector2(0f, Mathf.Sign(rawInput.y));
+                _moveDirection = new Vector2Int(0, (int)Mathf.Sign(rawInput.y));
             }
             else if (Mathf.Abs(rawInput.x) > 0.01f)
             {
-                _moveInput = new Vector2(Mathf.Sign(rawInput.x), 0f);
+                _moveDirection = new Vector2Int((int)Mathf.Sign(rawInput.x), 0);
             }
             else
             {
-                _moveInput = Vector2.zero;
+                _moveDirection = Vector2Int.zero;
             }
 
-            if (_moveInput.sqrMagnitude > 0.01f)
+            if (_moveDirection != Vector2Int.zero)
             {
-                _facingDirection = _moveInput.normalized;
+                _facingDirection = _moveDirection;
 
                 var targetCell = this.TargetCell();
 
@@ -259,7 +290,8 @@ namespace DontWaterMyBurrow.Player
 
         public void InteractWith()
         {
-            var objetive = transform.position + (Vector3)_facingDirection * _interactionDistance;
+            var facingDirection = new Vector3(_facingDirection.x, _facingDirection.y, 0);
+            var objetive = transform.position + facingDirection * _interactionDistance;
             var hit = Physics2D.OverlapCircle(objetive, 0.3f);
 
             if (hit)
@@ -331,6 +363,12 @@ namespace DontWaterMyBurrow.Player
 
             // TODO: Show all map
             if (_debugMode) Debug.Log("Map opened");
+        }
+        #endregion
+
+        public void SetMoveSpeed(float moveSpeed)
+        {
+            _currentMoveSpeed = moveSpeed;
         }
     }
 }
