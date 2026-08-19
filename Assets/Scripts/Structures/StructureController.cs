d36c09b073e28f22a47e0329421d4d5dc6171e5b
Jose A. Rojano Crisson <j053noir@gmail.com>
2026-08-19 11:53:13 -0500

    feat(structures): align structure center pivots and add editor grid snapping

--- Assets/Scripts/Structures/StructureController.cs
+++ Assets/Scripts/Structures/StructureController.cs
@@ -1,9 +1,9 @@
-using UnityEngine;
+using DontWaterMyBurrow.Building.Events;
 using DontWaterMyBurrow.Core;
 using DontWaterMyBurrow.Data;
-using DontWaterMyBurrow.Building.Events;
-using DontWaterMyBurrow.Structures.Events;
 using DontWaterMyBurrow.Hazards;
+using DontWaterMyBurrow.Structures.Events;
+using UnityEngine;
 
 namespace DontWaterMyBurrow.Structures
 {
@@ -11,24 +11,56 @@ namespace DontWaterMyBurrow.Structures
     [RequireComponent(typeof(Rigidbody2D))]
     public class StructureController : MonoBehaviour
     {
+        [Header("Config")]
+        [SerializeField] private MapGridConfigSO _mapGridConfig;
+        [SerializeField] private StructureDataSO _structureData;
+
+        [Header("Parameters")]
         [SerializeField] private StructureType _type;
-        [SerializeField] private StructureDataSO _dataSO;
         [SerializeField] private Vector2Int _position;
 
+        [Header("Health")]
+        [SerializeField] private bool _hasBeenDamaged = false;
         [SerializeField] private int _health;
         [SerializeField] private int _maxHealth;
-        [SerializeField] private bool _hasBeenDamaged = false;
 
-        public StructureType Type => _type;
-        public StructureDataSO DataSO => _dataSO;
-        public Vector2Int Position => _position;
         public bool IsDamaged => _hasBeenDamaged;
         public int Health => _health;
+        public StructureDataSO StructureData => _structureData;
+        public StructureType Type => _type;
+        public Vector2Int Position => _position;
+
+        private void Awake()
+        {
+            if (_mapGridConfig == null) Debug.LogError("[StructureController] MapGridConfigSO is null");
+            if (_structureData == null) Debug.LogError("[StructureController] StructureDataSO is null");
+        }
 
         private void OnEnable()
         {
-            _maxHealth = _dataSO.MaxHealth;
+            _maxHealth = _structureData.MaxHealth;
             _health = _maxHealth;
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_mapGridConfig != null && !Application.isPlaying)
+            {
+                var gridPosition = _mapGridConfig.WorldToGrid(transform.position);
+                transform.SetPositionAndRotation(
+                    _mapGridConfig.GridToWorld(gridPosition),
+                    _mapGridConfig.SnapToGridRotation(transform.rotation)
+                );
+            }
+        }
+#endif
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.TryGetComponent(out HazardsController hazard))
+            {
+                TakeDamage(hazard.DamageAmount);
+            }
         }
 
         public void Repair(int amount)
@@ -57,14 +89,6 @@ namespace DontWaterMyBurrow.Structures
             // TODO: Implement object pooling to about instancing new ones
             EventBus.Publish(new StructureDestroyedEvent(Position, gameObject));
             Destroy(this.gameObject);
-        }
-
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.TryGetComponent(out HazardsController hazard))
-            {
-                TakeDamage(hazard.DamageAmount);
-            }
         }
     }
 }
